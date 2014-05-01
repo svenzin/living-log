37a686f5e202e7e0aef9291869cbc046fa139896
svenzin <stephane.venzin@gmail.com>
2014-05-01 22:28:56 +0100

    More genericity

--- living-log/living-log-cli/Program.cs
+++ living-log/living-log-cli/Program.cs
@@ -43,10 +43,20 @@ namespace living_log_cli
         public KeyboardPressData(KeyPressEventArgsExt e) { Character = e.KeyChar; }
         public override string ToString() { return Character.ToString(); }
     }
+    public interface Category { string ToString(); }
+    public class Mouse_Move        : Category { public override string ToString() { return "Mouse_Move";        } }
+    public class Mouse_Down        : Category { public override string ToString() { return "Mouse_Down";        } }
+    public class Mouse_Up          : Category { public override string ToString() { return "Mouse_Up";          } }
+    public class Mouse_Click       : Category { public override string ToString() { return "Mouse_Click";       } }
+    public class Mouse_DoubleClick : Category { public override string ToString() { return "Mouse_DoubleClick"; } }
+    public class Mouse_Wheel       : Category { public override string ToString() { return "Mouse_Wheel";       } }
+    public class Keyboard_KeyDown  : Category { public override string ToString() { return "Keyboard_KeyDown";  } }
+    public class Keyboard_KeyUp    : Category { public override string ToString() { return "Keyboard_KeyUp";    } }
+    public class Keyboard_KeyPress : Category { public override string ToString() { return "Keyboard_KeyPress"; } }
     public class Activity
     {
         public long Timestamp;
-        public string Name;
+        public Category Type;
         public Data Info;
     }
     class Program
@@ -95,17 +105,17 @@ Options: -log LOG Uses the file LOG as log for the activity
             m_activityList = new List<Activity>();
 
             m_mouse = new MouseHookListener(new GlobalHooker());
-            m_mouse.MouseMoveExt += OnMouseMove;
-            m_mouse.MouseDownExt += OnMouseDown;
-            m_mouse.MouseUp += OnMouseUp;
-            m_mouse.MouseClickExt += OnMouseClick;
+            m_mouse.MouseMoveExt     += OnMouseMove;
+            m_mouse.MouseDownExt     += OnMouseDown;
+            m_mouse.MouseUp          += OnMouseUp;
+            m_mouse.MouseClickExt    += OnMouseClick;
             m_mouse.MouseDoubleClick += OnMouseDoubleClick;
-            m_mouse.MouseWheel += OnMouseWheel;
+            m_mouse.MouseWheel       += OnMouseWheel;
             m_mouse.Enabled = true;
             
             m_keyboard = new KeyboardHookListener(new GlobalHooker());
-            m_keyboard.KeyUp += OnKeyUp;
-            m_keyboard.KeyDown += OnKeyDown;
+            m_keyboard.KeyUp    += OnKeyUp;
+            m_keyboard.KeyDown  += OnKeyDown;
             m_keyboard.KeyPress += OnKeyPress;
             m_keyboard.Enabled = true;
 
@@ -117,57 +127,30 @@ Options: -log LOG Uses the file LOG as log for the activity
             m_filename = filename;
         }
 
-        private void Act(string name, Data info)
+        private void Act(Category type, Data info)
         {
-            m_activityList.Add(new Activity() { Timestamp = DateTime.UtcNow.Ticks, Name = name, Info = info });
+            m_activityList.Add(new Activity() { Timestamp = DateTime.UtcNow.Ticks, Type = type, Info = info });
         }
 
         private void OnTick(object sender, EventArgs e)
         {
             using (var file = new System.IO.StreamWriter(m_filename, true))
             {
-                m_activityList.ForEach((a) => { file.WriteLine(a.Timestamp.ToString() + " " + a.Name + " " + a.Info.ToString()); });
+                m_activityList.ForEach((a) => { file.WriteLine(a.Timestamp.ToString() + " " + a.Type.ToString() + " " + a.Info.ToString()); });
                 m_activityList.Clear();
             }
         }
 
-        private void OnMouseMove(object sender, MouseEventExtArgs e)
-        {
-            Act("MouseMove", new MouseMoveData(e));
-        }
-        private void OnMouseDown(object sender, MouseEventExtArgs e)
-        {
-            Act("MouseDown", new MouseButtonData(e));
-        }
-        private void OnMouseUp(object sender, MouseEventArgs e)
-        {
-            Act("MouseUp", new MouseButtonData(e as MouseEventExtArgs));
-        }
-        private void OnMouseClick(object sender, MouseEventExtArgs e)
-        {
-            Act("MouseClick", new MouseButtonData(e));
-        }
-        private void OnMouseDoubleClick(object sender, MouseEventArgs e)
-        {
-            Act("MouseDoubleClick", new MouseButtonData(e as MouseEventExtArgs));
-        }
-        private void OnMouseWheel(object sender, MouseEventArgs e)
-        {
-            Act("MouseWheel", new MouseWheelData(e as MouseEventExtArgs));
-        }
+        private void OnMouseMove       (object sender, MouseEventExtArgs e) { Act(new Mouse_Move(),        new MouseMoveData  (e));                      }
+        private void OnMouseDown       (object sender, MouseEventExtArgs e) { Act(new Mouse_Down(),        new MouseButtonData(e));                      }
+        private void OnMouseUp         (object sender, MouseEventArgs    e) { Act(new Mouse_Up(),          new MouseButtonData(e as MouseEventExtArgs)); }
+        private void OnMouseClick      (object sender, MouseEventExtArgs e) { Act(new Mouse_Click(),       new MouseButtonData(e));                      }
+        private void OnMouseDoubleClick(object sender, MouseEventArgs    e) { Act(new Mouse_DoubleClick(), new MouseButtonData(e as MouseEventExtArgs)); }
+        private void OnMouseWheel      (object sender, MouseEventArgs    e) { Act(new Mouse_Wheel(),       new MouseWheelData (e as MouseEventExtArgs)); }
 
-        private void OnKeyUp(object sender, System.Windows.Forms.KeyEventArgs e)
-        {
-            Act("KeyUp", new KeyboardKeyData(e as KeyEventArgsExt));
-        }
-        private void OnKeyDown(object sender, KeyEventArgs e)
-        {
-            Act("KeyDown", new KeyboardKeyData(e as KeyEventArgsExt));
-        }
-        private void OnKeyPress(object sender, KeyPressEventArgs e)
-        {
-            Act("KeyPress", new KeyboardPressData(e as KeyPressEventArgsExt));
-        }
+        private void OnKeyUp   (object sender, System.Windows.Forms.KeyEventArgs e) { Act(new Keyboard_KeyUp(),    new KeyboardKeyData  (e as KeyEventArgsExt));      }
+        private void OnKeyDown (object sender, KeyEventArgs                      e) { Act(new Keyboard_KeyDown(),  new KeyboardKeyData  (e as KeyEventArgsExt));      }
+        private void OnKeyPress(object sender, KeyPressEventArgs                 e) { Act(new Keyboard_KeyPress(), new KeyboardPressData(e as KeyPressEventArgsExt)); }
 
         Timer m_timer;
         string m_filename;
