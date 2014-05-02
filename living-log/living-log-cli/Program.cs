14b172147f98e1b56cdd0f3882a7735c91aa8b06
Stephane Venzin <stephane.venzin@gmail.com>
2014-05-02 17:30:33 +0100

    Serialize to binary

--- living-log/living-log-cli/Program.cs
+++ living-log/living-log-cli/Program.cs
@@ -8,56 +8,97 @@ using System.Threading.Tasks;
 
 using System.Windows.Forms;
 using System.Drawing;
+using System.IO;
 
 namespace living_log_cli
 {
-    public interface Data { string ToString(); }
-    public class MouseButtonData : Data
+    public interface IData
+    {
+        void WriteText(TextWriter writer);
+        void WriteBinary(BinaryWriter writer);
+        string ToString();
+    }
+    public class MouseButtonData : IData
     {
         public MouseButtons Button;
         public MouseButtonData(MouseEventExtArgs e) { Button = e.Button; }
         public override string ToString() { return Button.ToString(); }
+        public void WriteText(TextWriter writer) { writer.Write(ToString()); }
+        public void WriteBinary(BinaryWriter writer) { writer.Write((int)Button); }
     }
-    public class MouseWheelData : Data
+    public class MouseWheelData : IData
     {
         public int Delta;
         public MouseWheelData(MouseEventExtArgs e) { Delta = e.Delta; }
         public override string ToString() { return Delta.ToString(); }
+        public void WriteText(TextWriter writer) { writer.Write(ToString()); }
+        public void WriteBinary(BinaryWriter writer) { writer.Write(Delta); }
     }
-    public class MouseMoveData : Data
+    public class MouseMoveData : IData
     {
         public int X;
         public int Y;
         public MouseMoveData(MouseEventExtArgs e) { X = e.X; Y = e.Y; }
         public override string ToString() { return "(" + X.ToString() + ", " + Y.ToString() + ")"; }
+        public void WriteText(TextWriter writer) { writer.Write(ToString()); }
+        public void WriteBinary(BinaryWriter writer) { writer.Write(X); writer.Write(Y); }
     }
-    public class KeyboardKeyData : Data
+    public class KeyboardKeyData : IData
     {
         public Keys Key;
         public KeyboardKeyData(KeyEventArgsExt e) { Key = e.KeyCode; }
         public override string ToString() { return Key.ToString(); }
+        public void WriteText(TextWriter writer) { writer.Write(ToString()); }
+        public void WriteBinary(BinaryWriter writer) { writer.Write((int)Key); }
     }
-    public class KeyboardPressData : Data
+    public class KeyboardPressData : IData
     {
         public char Character;
         public KeyboardPressData(KeyPressEventArgsExt e) { Character = e.KeyChar; }
         public override string ToString() { return Character.ToString(); }
+        public void WriteText(TextWriter writer) { writer.Write(ToString()); }
+        public void WriteBinary(BinaryWriter writer) { writer.Write(Character); }
     }
-    public interface Category { string ToString(); }
-    public class Mouse_Move        : Category { public override string ToString() { return "Mouse_Move";        } }
-    public class Mouse_Down        : Category { public override string ToString() { return "Mouse_Down";        } }
-    public class Mouse_Up          : Category { public override string ToString() { return "Mouse_Up";          } }
-    public class Mouse_Click       : Category { public override string ToString() { return "Mouse_Click";       } }
-    public class Mouse_DoubleClick : Category { public override string ToString() { return "Mouse_DoubleClick"; } }
-    public class Mouse_Wheel       : Category { public override string ToString() { return "Mouse_Wheel";       } }
-    public class Keyboard_KeyDown  : Category { public override string ToString() { return "Keyboard_KeyDown";  } }
-    public class Keyboard_KeyUp    : Category { public override string ToString() { return "Keyboard_KeyUp";    } }
-    public class Keyboard_KeyPress : Category { public override string ToString() { return "Keyboard_KeyPress"; } }
+    public class SyncData : IData
+    {
+        public string Version;
+        public long Timestamp;
+        public override string ToString() { return Timestamp.ToString() + " " + Version; }
+        public void WriteText(TextWriter writer) { writer.Write(ToString()); }
+        public void WriteBinary(BinaryWriter writer) { writer.Write(Version); writer.Write(Timestamp); }
+    }
+    public interface ICategory { int GetId(); string ToString(); }
+    public class LivingLog_Startup : ICategory { public int GetId() { return 0; } public override string ToString() { return GetId() + " LivingLog_Startup"; } }
+    public class Mouse_Move        : ICategory { public int GetId() { return 1; } public override string ToString() { return GetId() + " Mouse_Move";        } }
+    public class Mouse_Down        : ICategory { public int GetId() { return 2; } public override string ToString() { return GetId() + " Mouse_Down";        } }
+    public class Mouse_Up          : ICategory { public int GetId() { return 3; } public override string ToString() { return GetId() + " Mouse_Up";          } }
+    public class Mouse_Click       : ICategory { public int GetId() { return 4; } public override string ToString() { return GetId() + " Mouse_Click";       } }
+    public class Mouse_DoubleClick : ICategory { public int GetId() { return 5; } public override string ToString() { return GetId() + " Mouse_DoubleClick"; } }
+    public class Mouse_Wheel       : ICategory { public int GetId() { return 6; } public override string ToString() { return GetId() + " Mouse_Wheel";       } }
+    public class Keyboard_KeyDown  : ICategory { public int GetId() { return 7; } public override string ToString() { return GetId() + " Keyboard_KeyDown";  } }
+    public class Keyboard_KeyUp    : ICategory { public int GetId() { return 8; } public override string ToString() { return GetId() + " Keyboard_KeyUp";    } }
+    public class Keyboard_KeyPress : ICategory { public int GetId() { return 9; } public override string ToString() { return GetId() + " Keyboard_KeyPress"; } }
     public class Activity
     {
         public long Timestamp;
-        public Category Type;
-        public Data Info;
+        public ICategory Type;
+        public IData Info;
+
+        public void WriteText(TextWriter writer)
+        {
+            writer.Write(Timestamp);
+            writer.Write(" ");
+            writer.Write(Type.ToString());
+            writer.Write(" ");
+            Info.WriteText(writer);
+        }
+
+        public void WriteBinary(BinaryWriter writer)
+        {
+            writer.Write(Timestamp);
+            writer.Write(Type.GetId());
+            Info.WriteBinary(writer);
+        }
     }
     class Program
     {
@@ -104,6 +145,8 @@ Options: -log LOG Uses the file LOG as log for the activity
         {
             m_activityList = new List<Activity>();
 
+            Act(new LivingLog_Startup(), new SyncData() { Timestamp = DateTime.UtcNow.Ticks, Version = "0.1" });
+
             m_mouse = new MouseHookListener(new GlobalHooker());
             m_mouse.MouseMoveExt     += OnMouseMove;
             m_mouse.MouseDownExt     += OnMouseDown;
@@ -127,18 +170,29 @@ Options: -log LOG Uses the file LOG as log for the activity
             m_filename = filename;
         }
 
-        private void Act(Category type, Data info)
+        private void Act(ICategory type, IData info)
         {
             m_activityList.Add(new Activity() { Timestamp = DateTime.UtcNow.Ticks, Type = type, Info = info });
         }
 
         private void OnTick(object sender, EventArgs e)
         {
-            using (var file = new System.IO.StreamWriter(m_filename, true))
+            Console.WriteLine("Writing " + m_activityList.Count + " activities");
+            using (var file = new BinaryWriter(new FileStream(m_filename, FileMode.Append)))
             {
-                m_activityList.ForEach((a) => { file.WriteLine(a.Timestamp.ToString() + " " + a.Type.ToString() + " " + a.Info.ToString()); });
+                WriteBinary(file);
                 m_activityList.Clear();
             }
+        }
+
+        private void WriteText(TextWriter text)
+        {
+            m_activityList.ForEach((a) => { a.WriteText(text); text.WriteLine(); });
+        }
+
+        private void WriteBinary(BinaryWriter data)
+        {
+            m_activityList.ForEach((a) => { a.WriteBinary(data); });
         }
 
         private void OnMouseMove       (object sender, MouseEventExtArgs e) { Act(new Mouse_Move(),        new MouseMoveData  (e));                      }
