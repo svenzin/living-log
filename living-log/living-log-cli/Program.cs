aee98ac36719bcd067ea4bf6dccf4acc64d01a93
svenzin <stephane.venzin@gmail.com>
2014-05-03 23:48:40 +0100

    Chose a data format

--- living-log/living-log-cli/Program.cs
+++ living-log/living-log-cli/Program.cs
@@ -12,10 +12,27 @@ using System.IO;
 
 namespace living_log_cli
 {
+    public class Constants
+    {
+        public static long TicksPerMs = TimeSpan.TicksPerMillisecond;
+        public static int DumpDelayInMs = 60 * 1000; // 1 minute
+        public static int SyncDelayInMs = 60 * 60 * 1000; // 1 hour
+        public static string SyncFormat = "yyyy-MM-dd_HH:mm:ss.fff";
+    }
+    public struct Timestamp
+    {
+        public long Milliseconds;
+
+        public Timestamp(long milliseconds) { Milliseconds = milliseconds; }
+        public Timestamp(DateTime time) : this(time.Ticks / Constants.TicksPerMs) { }
+
+        public override string ToString() { return Milliseconds.ToString(); }
+
+        public static Timestamp operator +(Timestamp a, Timestamp b) { return new Timestamp(a.Milliseconds + b.Milliseconds); }
+        public static Timestamp operator -(Timestamp a, Timestamp b) { return new Timestamp(a.Milliseconds - b.Milliseconds); }
+    }
     public interface IData
     {
-        void WriteText(TextWriter writer);
-        void WriteBinary(BinaryWriter writer);
         string ToString();
     }
     public class MouseButtonData : IData
@@ -23,103 +40,100 @@ namespace living_log_cli
         public MouseButtons Button;
         public MouseButtonData(MouseEventExtArgs e) { Button = e.Button; }
         public override string ToString() { return Button.ToString(); }
-        public void WriteText(TextWriter writer) { writer.Write(ToString()); }
-        public void WriteBinary(BinaryWriter writer) { writer.Write((int)Button); }
     }
     public class MouseWheelData : IData
     {
         public int Delta;
         public MouseWheelData(MouseEventExtArgs e) { Delta = e.Delta; }
         public override string ToString() { return Delta.ToString(); }
-        public void WriteText(TextWriter writer) { writer.Write(ToString()); }
-        public void WriteBinary(BinaryWriter writer) { writer.Write(Delta); }
     }
     public class MouseMoveData : IData
     {
         public int X;
         public int Y;
         public MouseMoveData(MouseEventExtArgs e) { X = e.X; Y = e.Y; }
-        public override string ToString() { return "(" + X.ToString() + ", " + Y.ToString() + ")"; }
-        public void WriteText(TextWriter writer) { writer.Write(ToString()); }
-        public void WriteBinary(BinaryWriter writer) { writer.Write(X); writer.Write(Y); }
+        public override string ToString() { return X.ToString() + " " + Y.ToString(); }
     }
     public class KeyboardKeyData : IData
     {
         public Keys Key;
         public KeyboardKeyData(KeyEventArgsExt e) { Key = e.KeyCode; }
         public override string ToString() { return Key.ToString(); }
-        public void WriteText(TextWriter writer) { writer.Write(ToString()); }
-        public void WriteBinary(BinaryWriter writer) { writer.Write((int)Key); }
     }
     public class KeyboardPressData : IData
     {
         public char Character;
         public KeyboardPressData(KeyPressEventArgsExt e) { Character = e.KeyChar; }
         public override string ToString() { return Character.ToString(); }
-        public void WriteText(TextWriter writer) { writer.Write(ToString()); }
-        public void WriteBinary(BinaryWriter writer) { writer.Write(Character); }
     }
     public class SyncData : IData
     {
         public string Version;
-        public long Timestamp;
-        public override string ToString() { return Timestamp.ToString() + " " + Version; }
-        public void WriteText(TextWriter writer) { writer.Write(ToString()); }
-        public void WriteBinary(BinaryWriter writer) { writer.Write(Version); writer.Write(Timestamp); }
+        public DateTime Timestamp;
+        public override string ToString() { return Timestamp.ToString(Constants.SyncFormat) + " " + Version; }
     }
-    public interface ICategory { int GetId(); string ToString(); }
-    public class LivingLog_Startup : ICategory { public int GetId() { return 0; } public override string ToString() { return GetId() + " LivingLog_Startup"; } }
-    public class Mouse_Move        : ICategory { public int GetId() { return 1; } public override string ToString() { return GetId() + " Mouse_Move";        } }
-    public class Mouse_Down        : ICategory { public int GetId() { return 2; } public override string ToString() { return GetId() + " Mouse_Down";        } }
-    public class Mouse_Up          : ICategory { public int GetId() { return 3; } public override string ToString() { return GetId() + " Mouse_Up";          } }
-    public class Mouse_Click       : ICategory { public int GetId() { return 4; } public override string ToString() { return GetId() + " Mouse_Click";       } }
-    public class Mouse_DoubleClick : ICategory { public int GetId() { return 5; } public override string ToString() { return GetId() + " Mouse_DoubleClick"; } }
-    public class Mouse_Wheel       : ICategory { public int GetId() { return 6; } public override string ToString() { return GetId() + " Mouse_Wheel";       } }
-    public class Keyboard_KeyDown  : ICategory { public int GetId() { return 7; } public override string ToString() { return GetId() + " Keyboard_KeyDown";  } }
-    public class Keyboard_KeyUp    : ICategory { public int GetId() { return 8; } public override string ToString() { return GetId() + " Keyboard_KeyUp";    } }
-    public class Keyboard_KeyPress : ICategory { public int GetId() { return 9; } public override string ToString() { return GetId() + " Keyboard_KeyPress"; } }
+    
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public int GetId() { return Id; }
+        public override string ToString() { return Id.ToString() + " " + Name; }
+
+        public static Category LivingLog_Startup = new Category() { Id = 0, Name = "LivingLog_Startup" };
+        public static Category LivingLog_Sync    = new Category() { Id =10, Name = "LivingLog_Sync"    };
+        public static Category Mouse_Move        = new Category() { Id = 1, Name = "Mouse_Move"        };
+        public static Category Mouse_Down        = new Category() { Id = 2, Name = "Mouse_Down"        };
+        public static Category Mouse_Up          = new Category() { Id = 3, Name = "Mouse_Up"          };
+        public static Category Mouse_Click       = new Category() { Id = 4, Name = "Mouse_Click"       };
+        public static Category Mouse_DoubleClick = new Category() { Id = 5, Name = "Mouse_DoubleClick" };
+        public static Category Mouse_Wheel       = new Category() { Id = 6, Name = "Mouse_Wheel"       };
+        public static Category Keyboard_KeyDown  = new Category() { Id = 7, Name = "Keyboard_KeyDown"  };
+        public static Category Keyboard_KeyUp    = new Category() { Id = 8, Name = "Keyboard_KeyUp"    };
+        public static Category Keyboard_KeyPress = new Category() { Id = 9, Name = "Keyboard_KeyPress" };
+    }
     public class Activity
     {
-        public long Timestamp;
-        public ICategory Type;
+        public Timestamp Timestamp;
+        public Category Type;
         public IData Info;
-
-        public void WriteText(TextWriter writer)
-        {
-            writer.Write(Timestamp);
-            writer.Write(" ");
-            writer.Write(Type.ToString());
-            writer.Write(" ");
-            Info.WriteText(writer);
-        }
-
-        public void WriteBinary(BinaryWriter writer)
-        {
-            writer.Write(Timestamp);
-            writer.Write(Type.GetId());
-            Info.WriteBinary(writer);
-        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            string filename;
-            if (args.Length == 0)
+            string filename = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\living-log.log";
+            int index = 0;
+            while (index < args.Length)
             {
-                filename = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\living-log.log";
-            }
-            else if (args.Length == 2 && args[0] == "-log")
-            {
-                filename = args[1];
-            }
-            else
-            {
-                Help();
-                return;
+                if (index + 2 <= args.Length)
+                {
+                    switch (args[index])
+                    {
+                        case "-log":
+                            filename = args[index + 1];
+                            break;
+                        case "-delay":
+                            Constants.DumpDelayInMs = 1000 * Convert.ToInt32(args[index + 1]);
+                            break;
+                        case "-sync":
+                            Constants.SyncDelayInMs = 1000 * Convert.ToInt32(args[index + 1]);
+                            break;
+                        default:
+                            Help();
+                            return;
+                    }
+                    index += 2;
+                }
+                else
+                {
+                    Help();
+                    return;
+                }
             }
 
-            Console.WriteLine("Using " + filename + " as log file");
+            Console.WriteLine("Using " + filename + " as log file"); 
 
             System.Diagnostics.Process.GetCurrentProcess().PriorityClass = System.Diagnostics.ProcessPriorityClass.BelowNormal;
             Program p = new Program(filename);
@@ -134,10 +148,15 @@ living-log-cli
 
 Starts a keyboard and mouse activity logger
 
-Command: living-log-cli [-log LOG]
+Command: living-log-cli [-log LOG -delay DELAY -sync DELAY]
 
-Options: -log LOG Uses the file LOG as log for the activity
-                  Default is ""My Documents""\living-log.log"
+Options: -log LOG     Uses the file LOG as log for the activity
+                      Default is ""My Documents""\living-log.log
+         -delay DELAY Delay in seconds between each dump to the log file
+                      Default is each minute (60s)
+         -sync DELAY  Delay in seconds between each sync message in the dump
+                      Default is each hour (3600s)
+"
             );
         }
         
@@ -145,73 +164,75 @@ Options: -log LOG Uses the file LOG as log for the activity
         {
             m_activityList = new List<Activity>();
 
-            Act(new LivingLog_Startup(), new SyncData() { Timestamp = DateTime.UtcNow.Ticks, Version = "0.1" });
+            m_previous = new Timestamp(DateTime.UtcNow);
+            Act(Category.LivingLog_Startup, new SyncData() { Timestamp = DateTime.UtcNow, Version = "1" });
 
             m_mouse = new MouseHookListener(new GlobalHooker());
-            m_mouse.MouseMoveExt     += OnMouseMove;
-            m_mouse.MouseDownExt     += OnMouseDown;
-            m_mouse.MouseUp          += OnMouseUp;
-            m_mouse.MouseClickExt    += OnMouseClick;
-            m_mouse.MouseDoubleClick += OnMouseDoubleClick;
-            m_mouse.MouseWheel       += OnMouseWheel;
+            m_mouse.MouseMoveExt     += (s, e) => { Act(Category.Mouse_Move,        new MouseMoveData  (e)); };
+            m_mouse.MouseDownExt     += (s, e) => { Act(Category.Mouse_Down,        new MouseButtonData(e)); };
+            m_mouse.MouseUp          += (s, e) => { Act(Category.Mouse_Up,          new MouseButtonData(e as MouseEventExtArgs)); };
+            m_mouse.MouseClickExt    += (s, e) => { Act(Category.Mouse_Click,       new MouseButtonData(e)); };
+            m_mouse.MouseDoubleClick += (s, e) => { Act(Category.Mouse_DoubleClick, new MouseButtonData(e as MouseEventExtArgs)); };
+            m_mouse.MouseWheel       += (s, e) => { Act(Category.Mouse_Wheel,       new MouseWheelData (e as MouseEventExtArgs)); };
             m_mouse.Enabled = true;
             
             m_keyboard = new KeyboardHookListener(new GlobalHooker());
-            m_keyboard.KeyUp    += OnKeyUp;
-            m_keyboard.KeyDown  += OnKeyDown;
-            m_keyboard.KeyPress += OnKeyPress;
+            m_keyboard.KeyUp    += (s, e) => { Act(Category.Keyboard_KeyUp,    new KeyboardKeyData  (e as KeyEventArgsExt)); };
+            m_keyboard.KeyDown  += (s, e) => { Act(Category.Keyboard_KeyDown,  new KeyboardKeyData  (e as KeyEventArgsExt)); };
+            m_keyboard.KeyPress += (s, e) => { Act(Category.Keyboard_KeyPress, new KeyboardPressData(e as KeyPressEventArgsExt)); };
             m_keyboard.Enabled = true;
 
-            m_timer = new Timer();
-            m_timer.Interval = 1000 * 60; // 1 minute
-            m_timer.Tick += OnTick;
-            m_timer.Enabled = true;
+            m_dumpTimer = new Timer();
+            m_dumpTimer.Interval = Constants.DumpDelayInMs;
+            m_dumpTimer.Tick += OnTick;
+            m_dumpTimer.Enabled = true;
+
+            m_syncTimer = new Timer();
+            m_syncTimer.Interval = Constants.SyncDelayInMs;
+            m_syncTimer.Tick += (s, e) => { Act(Category.LivingLog_Sync, new SyncData() { Timestamp = DateTime.UtcNow, Version = "1" }); };
+            m_syncTimer.Enabled = true;
 
             m_filename = filename;
         }
 
-        private void Act(ICategory type, IData info)
+        private void Act(Category type, IData info)
         {
-            m_activityList.Add(new Activity() { Timestamp = DateTime.UtcNow.Ticks, Type = type, Info = info });
+            m_activityList.Add(new Activity() { Timestamp = new Timestamp(DateTime.UtcNow), Type = type, Info = info });
         }
 
         private void OnTick(object sender, EventArgs e)
         {
             Console.WriteLine("Writing " + m_activityList.Count + " activities");
-            using (var file = new BinaryWriter(new FileStream(m_filename, FileMode.Append)))
+            using (var writer = new StreamWriter(new FileStream(m_filename, FileMode.Append)))
             {
-                WriteBinary(file);
+                WriteText(writer);
                 m_activityList.Clear();
             }
         }
 
-        private void WriteText(TextWriter text)
+        private void WriteText(TextWriter writer)
         {
-            m_activityList.ForEach((a) => { a.WriteText(text); text.WriteLine(); });
+            m_activityList.ForEach((a) =>
+            {
+                writer.Write(a.Timestamp - m_previous);
+                writer.Write(" ");
+                writer.Write(a.Type.Id);
+                writer.Write(" ");
+                writer.Write(a.Info.ToString());
+                writer.WriteLine();
+
+                m_previous = a.Timestamp;
+            });
         }
 
-        private void WriteBinary(BinaryWriter data)
-        {
-            m_activityList.ForEach((a) => { a.WriteBinary(data); });
-        }
-
-        private void OnMouseMove       (object sender, MouseEventExtArgs e) { Act(new Mouse_Move(),        new MouseMoveData  (e));                      }
-        private void OnMouseDown       (object sender, MouseEventExtArgs e) { Act(new Mouse_Down(),        new MouseButtonData(e));                      }
-        private void OnMouseUp         (object sender, MouseEventArgs    e) { Act(new Mouse_Up(),          new MouseButtonData(e as MouseEventExtArgs)); }
-        private void OnMouseClick      (object sender, MouseEventExtArgs e) { Act(new Mouse_Click(),       new MouseButtonData(e));                      }
-        private void OnMouseDoubleClick(object sender, MouseEventArgs    e) { Act(new Mouse_DoubleClick(), new MouseButtonData(e as MouseEventExtArgs)); }
-        private void OnMouseWheel      (object sender, MouseEventArgs    e) { Act(new Mouse_Wheel(),       new MouseWheelData (e as MouseEventExtArgs)); }
-
-        private void OnKeyUp   (object sender, System.Windows.Forms.KeyEventArgs e) { Act(new Keyboard_KeyUp(),    new KeyboardKeyData  (e as KeyEventArgsExt));      }
-        private void OnKeyDown (object sender, KeyEventArgs                      e) { Act(new Keyboard_KeyDown(),  new KeyboardKeyData  (e as KeyEventArgsExt));      }
-        private void OnKeyPress(object sender, KeyPressEventArgs                 e) { Act(new Keyboard_KeyPress(), new KeyboardPressData(e as KeyPressEventArgsExt)); }
-
-        Timer m_timer;
+        Timer m_syncTimer;
+        Timer m_dumpTimer;
         string m_filename;
 
         MouseHookListener m_mouse;
         KeyboardHookListener m_keyboard;
 
+        Timestamp m_previous;
         List<Activity> m_activityList;
     }
 }
