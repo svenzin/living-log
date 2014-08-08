854e398a4385687b6f18d185d24a5b2c661292ad
Stephane Venzin <stephane.venzin@gmail.com>
2014-08-08 17:24:47 +0100

    Basic command parsing

--- living-log/living-gps-cli/Program.cs
+++ living-log/living-gps-cli/Program.cs
@@ -7,7 +7,57 @@ using System.Threading.Tasks;
 
 namespace living_gps_cli
 {
-    
+    interface ICommandMatcher
+    {
+        bool IsMatch(string command);
+        Func<string> GetCommand(string command);
+    }
+    class CommandEmpty : ICommandMatcher
+    {
+        public static Func<string> EmptyCommand = () => { return string.Empty; };
+        public bool IsMatch(string command) { return command.Equals(string.Empty); }
+        public Func<string> GetCommand(string command) { return EmptyCommand; }
+    }
+    class CommandGarmin : ICommandMatcher
+    {
+        class CommandHelp : ICommandMatcher
+        {
+            public bool IsMatch(string command) { return command.Equals("help"); }
+            public Func<string> GetCommand(string command) { return () => { return "Garmin commands : version" + Environment.NewLine; }; }
+        }
+        class CommandVersion : ICommandMatcher
+        {
+            public bool IsMatch(string command) { return command.Equals("version"); }
+            public Func<string> GetCommand(string command) { return () => { return "Version 0.1" + Environment.NewLine; }; }
+        }
+
+        private List<ICommandMatcher> CommandList;
+        public CommandGarmin()
+        {
+            CommandList = new List<ICommandMatcher>();
+            CommandList.Add(new CommandHelp());
+            CommandList.Add(new CommandVersion());
+        }
+        public bool IsMatch(string command)
+        {
+            return command.StartsWith("garmin ");
+        }
+
+        public Func<string> GetCommand(string command)
+        {
+            string garminCommand = command.Substring("garmin ".Length);
+            ICommandMatcher match = CommandList.Find((matcher) => { return matcher.IsMatch(garminCommand); });
+            if (match != null)
+            {
+                return match.GetCommand(garminCommand);
+            }
+            else
+            {
+                return CommandEmpty.EmptyCommand;
+            }
+        }
+    }
+
     class Program
     {
         #region Command line arguments
@@ -41,26 +91,46 @@ namespace living_gps_cli
         private TextReader Input;
         private TextWriter Output;
         private bool IsRunning;
-        
+
+        class CommandExit : ICommandMatcher
+        {
+            private Program Repl;
+            public CommandExit(Program repl) { Repl = repl; }
+            public bool IsMatch(string command)
+            {
+                return command.Equals("exit");
+            }
+
+            public Func<string> GetCommand(string command)
+            {
+                return () => { Repl.IsRunning = false; return "Exiting..." + Environment.NewLine; };
+            }
+        }
+
         Program(TextReader input, TextWriter output)
         {
             IsRunning = true;
             Input = input;
             Output = output;
+
+            CommandList = new List<ICommandMatcher>();
+            CommandList.Add(new CommandExit(this));
+            CommandList.Add(new CommandGarmin());
         }
 
+        private List<ICommandMatcher> CommandList;
         Func<string> Read()
         {
-            string command = Input.ReadLine();
-            if (command.Trim().ToLower().Equals("exit"))
+            string line = Input.ReadLine().Trim();
+            ICommandMatcher command = CommandList.Find((item) => { return item.IsMatch(line); });
+            if (command != null)
             {
-                return () =>
-                {
-                    IsRunning = false;
-                    return "Exiting..." + System.Environment.NewLine;
-                };
+                return command.GetCommand(line);
             }
-            return () => { return string.Empty; };
+            else
+            {
+                return CommandEmpty.EmptyCommand;
+            }
         }
         
         void REPL()
